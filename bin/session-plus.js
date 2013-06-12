85c197b67066ca30741b4526e7ace54b4b24b74a
DicaxDorcas <github@rosswalker.me>
2013-06-12 17:21:22 +0100

    Updated package and added shell to main file.

---
+++ bin/session-plus.js
@@ -0,0 +1,17 @@
+exports.start = function (req, res) {
+    // Starts session
+
+    return
+}
+
+exports.end = function (req, res) {
+    // Ends session
+
+    return
+}
+
+exports.kill = function (req, res) {
+    // Kills session
+
+    return
+}
